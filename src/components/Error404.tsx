bcc12aa6c63f379b634c83a6ab8b9fa4b87502aa
vlad <39403327+itadan@users.noreply.github.com>
2024-10-20 12:58:48 +0300

    4

--- src/components/Error404.tsx
+++ src/components/Error404.tsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 export default function Error404() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1 style={{ fontSize: '30px', color: 'black', padding: '20px' }}>404 - Page Not Found</h1>
-      <p style={{ fontSize: '15px', color: 'black', padding: '10px' }}>Something went wrong</p>
-      <p style={{ fontSize: '15px', color: 'black', padding: '10px' }}>Try again later</p>
+      <h1 style={{ fontSize: '30px', color: 'black', padding: '21px' }}>404 - Page Not Found</h1>
+      <p style={{ fontSize: '15px', color: 'black', padding: '11px' }}>Something went wrong</p>
+      <p style={{ fontSize: '15px', color: 'black', padding: '11px' }}>Try again later</p>
       <Link to='/' style={{ fontSize: '15px', color: 'black', padding: '10px' }}>Back to main page</Link>
     </div>
   );
